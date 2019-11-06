03a33515eaca6dd3869270c45c4fac922d6ca551
seasons986 <annalin12345@gmail.com>
2019-11-05 23:41:13 -0800

    Adding books to the shelf

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,3 +1,8 @@
+function norm(x, y1, y2, toString) {
+    const r = (x / y1) % y2;
+    return toString ? (r.toString() + 'px') : r;
+}
+
 function getSizeOfBook(pages) {
     switch (true) {
         case pages > 750:
@@ -19,30 +24,35 @@ function getAndRenderBooks() {
 
     request.onload = () => {
         request.response['bookItems'].forEach((bookObject) => {
-            const book = {
-                'title': bookObject['title'],
-                'author': bookObject['author'],
-                'pages': bookObject['pages'],
-                'size': getSizeOfBook(bookObject['pages'])
-            };
-            renderBooks(book);
+            if (bookObject) {
+                const book = {
+                    'title': bookObject['title'],
+                    'author': bookObject['author'],
+                    'pages': bookObject['pages'],
+                    'size': getSizeOfBook(bookObject['pages'])
+                };
+                renderBooks(book);
+            }
         });
     };
     request.send();
 }
 
 function renderBooks(item) {
+    const canvas = document.getElementById('canvas');
+    const canvasHeight = canvas.clientHeight;
     const element = document.createElement('div');
-    const titleElement = document.createElement('span');
+    const titleElement = document.createElement('p');
     const authorElement = document.createElement('span');
+    titleElement.classList.add('title');
+    authorElement.classList.add('author');
     titleElement.innerHTML = item.title;
     authorElement.innerHTML = item.author;
     element.appendChild(titleElement);
     element.appendChild(authorElement);
-    element.style.width = (item.pages / 12).toString() + 'px';
+    element.style.fontSize = norm(item.pages, 60, canvasHeight, true);
     element.classList += ('book ' + item.size);
-    document.getElementById('canvas').appendChild(element);
-    //meow
+    canvas.appendChild(element);
 }
 
 function init() {
